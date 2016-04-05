4b4cfead73187b6a624a7e1ff5658333b63a5681
zorro76 <sergey.martynuk@gmail.com>
2016-04-05 09:52:32 +0300

    commit 05.01.16

---
+++ js/common.js
@@ -0,0 +1,102 @@
+/**
+ * Created by martynuk on 04.04.16.
+ */
+
+$(document).ready(function() {
+    $('#registrationForm').bootstrapValidator({
+        framework: 'bootstrap',
+        icon: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            firstName: {
+                row: '.col-xs-4',
+                validators: {
+                    notEmpty: {
+                        message: 'The first name is required'
+                    }
+                }
+            },
+            lastName: {
+                row: '.col-xs-4',
+                validators: {
+                    notEmpty: {
+                        message: 'The last name is required'
+                    }
+                }
+            },
+            username: {
+                validators: {
+                    notEmpty: {
+                        message: 'The username is required'
+                    },
+                    stringLength: {
+                        min: 6,
+                        max: 30,
+                        message: 'The username must be more than 6 and less than 30 characters long'
+                    },
+                    regexp: {
+                        regexp: /^[a-zA-Z0-9_\.]+$/,
+                        message: 'The username can only consist of alphabetical, number, dot and underscore'
+                    }
+                }
+            },
+            email: {
+                validators: {
+                    notEmpty: {
+                        message: 'The email address is required'
+                    },
+                    emailAddress: {
+                        message: 'The input is not a valid email address'
+                    }
+                }
+            },
+            password: {
+                validators: {
+                    notEmpty: {
+                        message: 'The password is required'
+                    },
+                    different: {
+                        field: 'name',
+                        message: 'The password cannot be the same as username'
+                    }
+                }
+            },
+            gender: {
+                validators: {
+                    notEmpty: {
+                        message: 'The gender is required'
+                    }
+                }
+            },
+            birthday: {
+                validators: {
+                    notEmpty: {
+                        message: 'The date of birth is required'
+                    },
+                    date: {
+                        format: 'YYYY/MM/DD',
+                        message: 'The date of birth is not valid'
+                    }
+                }
+            }
+        }
+    });
+
+    $('input[type=checkbox]').css({'opacity': 0});
+
+    $('input[type=checkbox]').css({'opacity': 0}).wrap('<span class="wrap-checkbox"></span>');
+
+    $('.wrap-checkbox').click(function() {
+        $('.wrap-checkbox').removeClass("active");
+        $(this).addClass("active");
+    });
+
+    $("#login-group").on("click", ".login", function(){
+        $("#login-group .login").removeClass("active");
+        $(this).addClass("active");
+    });
+    $('[data-toggle="tooltip"]').tooltip();
+});
